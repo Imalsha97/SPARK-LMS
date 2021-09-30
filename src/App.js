0708a7b5e4f4af5f02fb1518b1fc755faf707a48
Imalsha <imalsham092@gmail.com>
2021-09-29 19:47:14 -0700

    create Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-import {Header, Main, Footer} from  "./components/Latout"
+import React from "react";
+import {Header, Main, Footer} from  "./components/Layout";
+import { NavBar, NavItem ,NavLink} from './components/Navbar';
 function App() {
   return (
     <>
-    <Header>header</Header>
+    <Header><NavBar>
+      <NavItem href="#">
+        <NavLink>
+          Catalog
+        </NavLink>
+
+      </NavItem>
+      <NavItem href="#">
+        <NavLink>
+          Dashboard
+        </NavLink>
+
+      </NavItem>
+      </NavBar></Header>
    <Main>main</Main>
    <Footer>footer</Footer>
     </>
